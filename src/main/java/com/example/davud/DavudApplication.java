883f453646eae75331252387219df534bf53e332
Macbook Air <thesame@email.com>
2021-04-15 12:05:51 +0400

    Initial commit

---
+++ src/main/java/com/example/davud/DavudApplication.java
@@ -0,0 +1,14 @@
+package com.example.davud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavudApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(DavudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/davud/DavudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.davud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
